3f35a015b3d5c329abff6a8ef39df2a207c940c9
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-09-07 12:13:58 +0200

    second commit

--- nameparts.js
+++ nameparts.js
@@ -1,11 +1,33 @@
 "use strict";
 
+// const fullName = "Louise Moesgaard Nielsen";
+
+// const splitNames = fullName.split(" ");
+
+
+
+// console.log(splitNames[0]);
+// console.log(splitNames[1]);
+// console.log(splitNames[2]);
+
+
+// const firstName = fullName.substring(0, fullName.indexOf(" "));
+// const middleName = fullName.substring(
+//   firstName.length + 1,
+//   fullName.lastIndexOf(" ")
+// );
+// const lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
+
+
+// console.log(firstName, middleName, lastname);
+
 const fullName = "Louise Moesgaard Nielsen";
+getNameParts(fullName);
 
-const splitNames = fullName.split(" ");
+function getNameParts(fullName){
+    let firstName = fullName.substring(0, fullName.indexOf(" "));
+    let middleName = fullName.substring(firstName.length + 1,fullName.lastIndexOf(" "));
+    let lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
 
-
-
-console.log(splitNames[0]);
-console.log(splitNames[1]);
-console.log(splitNames[2]);
+    console.log({firstName, middleName, lastName});
+}
